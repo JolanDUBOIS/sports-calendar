2a0f9cdd22155091f4edfd0b71dbfe4233a03c8e
Jolan <jolan11.dubois@gmail.com>
2025-02-02 17:57:06 +0100

    feat: added calendar creation tools

---
+++ src/calendar_creation/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from src.calendar_creation.calendar import FootballCalendar
---
+++ src/calendar_creation/calendar.py
@@ -0,0 +1,130 @@
+import pytz
+from pathlib import Path
+from datetime import datetime, timedelta
+
+import pandas as pd
+from icalendar import Calendar, Event
+
+from src.calendar_creation import logger
+
+
+class FootballCalendar:
+    """ TODO - ics oriented """
+
+    def __init__(self, calendar: Calendar=None, **kwargs):
+        """ TODO """
+        if calendar is None:
+            self.calendar = Calendar()
+        else:
+            self.calendar = calendar
+    
+    @property
+    def ics_content(self):
+        """ TODO """
+        return self.calendar.to_ical()
+
+    def __add_event(
+        self,
+        title: str,
+        utc_start: datetime,
+        utc_end: datetime,
+        location: str=None,
+        description: str=None,
+        **kwargs) -> None:
+        """ TODO """
+        assert isinstance(utc_start, datetime)
+        assert isinstance(utc_end, datetime)
+
+        event = Event()
+
+        event.add('summary', title)
+        utc_start = pytz.utc.localize(utc_start)
+        event.add('dtstart', utc_start.astimezone(pytz.utc))
+        utc_end = pytz.utc.localize(utc_end)
+        event.add('dtend', utc_end.astimezone(pytz.utc))
+        if location is not None:
+            event.add('location', location)
+        if description is not None:
+            event.add('description', description)
+        for key, value in kwargs:
+            try:
+                event.add(key, value)
+            except Exception as e:
+                logger.error(f"Couldn't add {value} to {key}.")
+        
+        self.calendar.add_component(event)
+    
+    def add_matches(self, matches: pd.DataFrame):
+        """ TODO """
+        for _, match in matches.iterrows():
+            self.add_match(
+                home_team=match['home_team_short_name'],
+                away_team=match['away_team_short_name'],
+                utc_date=match['utc_date'],
+                competition=match['competition_name'],
+                competition_code=match['competition_code'],   # TODO - optional
+                stage=match['stage']   # TODO - optional
+            )
+
+    def add_match(
+        self,
+        home_team: str,
+        away_team: str,
+        utc_date: str,
+        competition: str='',
+        competition_code: str='',
+        stage: str='',
+        stadium: str=''
+    ):
+        """ TODO - We recommend to use the short name instead of the full name """
+        title = self.get_match_title(home_team, away_team, competition_code)
+        match_start = datetime.strptime(utc_date, "%Y-%m-%dT%H:%M:%SZ")
+        match_end = match_start + timedelta(hours=2)
+        location = stadium
+        description = self.get_match_description(competition, stage)
+        self.__add_event(
+            title, 
+            match_start,
+            match_end, 
+            location,
+            description
+        )
+    
+    def save(self, file_path: Path):
+        """ TODO """
+        file_suffix = file_path.suffix
+        if file_suffix != '.ics':
+            logger.error("The file path must have a suffix '.ics'. Please provide a valid .ics file.")
+            return
+        with file_path.open(mode='wb') as ics_file:
+            ics_file.write(self.ics_content)
+
+    @staticmethod
+    def get_match_title(
+        home_team: str,
+        away_team: str,
+        competition_code: str='',
+        **kwargs
+    ) -> str:
+        """ TODO """
+        if competition_code == '':
+            return f"{home_team} - {away_team}"
+        else:
+            return f"{home_team} - {away_team} ({competition_code})"
+    
+    @staticmethod
+    def get_match_description(
+        competition: str, 
+        stage: str, 
+        **kwargs
+    ) -> str:
+        """ TODO """
+        description = f"Competition: {competition}"
+        if stage != '':
+            description += f"\nStage: {stage}"
+        return description
+
+    @staticmethod
+    def format_stage(stage: str) -> str:
+        """ TODO """
+        return stage.lower().replace('_', ' ').capitalize()
