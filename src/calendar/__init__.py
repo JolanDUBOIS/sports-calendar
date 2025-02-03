9a1070a99dc1a5f0647d64d666319e545c9cea2b
Jolan <jolan11.dubois@gmail.com>
2025-02-03 18:15:04 +0100

    update: updated football_calendar creation to match output of the new source

---
+++ src/calendar/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+from src.calendar.football_calendar import FootballCalendar
+from src.calendar.google_calendar_manager import GoogleCalendarManager
--- src/calendar_creation/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-from src.calendar_creation.calendar import FootballCalendar
