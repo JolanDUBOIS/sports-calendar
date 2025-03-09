f6fe993314cc65658defe0399042238852be5d9e
Jolan <jolan11.dubois@gmail.com>
2025-03-09 11:50:23 +0100

    chore: cleaned main workflow

--- src/__main__.py
+++ src/__main__.py
@@ -1,34 +1,7 @@
-import os
-from datetime import datetime
-from pathlib import Path
-
-from src import logger
-from src.selection import Selection
+from src.main import run_selection, test
 from src.cli_utils import parse_arguments
-from src.live_soccer_source import SoccerLiveScraper
-from src.calendar import FootballCalendar, GoogleCalendarManager
+from src.scraper_db import update_database
 
-
-def update_database():
-    logger.info("Updating database...")
-    soccer_live_scraper = SoccerLiveScraper(wait_time=20)
-    soccer_live_scraper.update_database()
-    logger.info("Database updated successfully")
-
-def run_selection(save_ics: bool=False):
-    logger.info("Running selection...")
-    selection_file_path = Path(os.getenv("SELECTION_FILE"))
-    database_path = Path("data")
-    selection = Selection.from_json(selection_file_path, database_path)
-    matches = selection.get_matches()
-    calendar = FootballCalendar()
-    calendar.add_matches(matches)
-    if save_ics:
-        now = datetime.now().strftime("%Y-%m-%d")
-        calendar.save_to_ics(database_path / "ics_calendars" / f"selection_calendar_{now}.ics")
-    google_calendar_manager = GoogleCalendarManager()
-    google_calendar_manager.add_calendar(calendar)
-    logger.info("Selection ran successfully")
 
 if __name__ == '__main__':
     args = parse_arguments()
@@ -37,8 +10,10 @@ if __name__ == '__main__':
         update_database()
     
     elif args.full_update:
-        # TODO 
         pass
 
     elif args.run_selection:
         run_selection(save_ics=True)
+
+    elif args.test:
+        test(args.test)
