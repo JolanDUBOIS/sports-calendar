29045499f6991490bbe6bbb4484c34d52a5c30c0
Jolan <jolan11.dubois@gmail.com>
2025-03-26 15:22:12 +0100

    feat: added fifa ranking scraping to workflow

--- src/scraper_db/__init__.py
+++ src/scraper_db/__init__.py
@@ -3,5 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 from .pipeline import update_database
-from .data_scraper import SoccerLiveScraper
+from .livesoccer_scraper import LiveSoccerScraper
+from .football_ranking_scraper import FootballRankingScraper
 from .database_manager import DatabaseManager
--- src/scraper_db/pipeline.py
+++ src/scraper_db/pipeline.py
@@ -1,22 +1,24 @@
 import traceback
 
 from src.scraper_db import logger
-from .data_scraper import SoccerLiveScraper
+from .livesoccer_scraper import LiveSoccerScraper
+from .football_ranking_scraper import FootballRankingScraper
 from .database_manager import DatabaseManager
 
 def update_database(competitions: list[str]=None):
     """ TODO """
     logger.info("Updating database...")
-    scraper = SoccerLiveScraper()
+    ls_scraper = LiveSoccerScraper()
+    fr_scraper = FootballRankingScraper()
     db = DatabaseManager()
 
     if competitions is None:
-        competitions = scraper.get_competitions()
+        competitions = ls_scraper.get_competitions()
 
     for competition in competitions:
         logger.info(f"Updating database for competition: {competition}")
         try:
-            matches, standings = scraper.get_competition(competition)
+            matches, standings = ls_scraper.get_competition(competition)
             if matches is not None:
                 db.insert("matches", matches)
                 logger.debug(f"Added matches for {competition} to database.")
@@ -26,4 +28,17 @@ def update_database(competitions: list[str]=None):
         except Exception as e:
             logger.error(f"Error while updating database for competition {competition}: {e}")
             logger.debug(traceback.format_exc())
+
+    try:
+        logger.info("Updating FIFA rankings...")
+        fifa_rankings = fr_scraper.get_fifa_rankings()
+        fifa_rankings["Competition"] = "FIFA Rankings"
+        fifa_rankings["Is League"] = True
+        if fifa_rankings is not None:
+            db.insert("standings", fifa_rankings)
+            logger.debug("Added FIFA rankings to database.")
+    except Exception as e:
+        logger.error(f"Error while updating FIFA rankings: {e}")
+        logger.debug(traceback.format_exc())
+
     logger.info("Database update complete.")
