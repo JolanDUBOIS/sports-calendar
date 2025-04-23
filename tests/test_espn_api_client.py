a2586545d0505fb76a634a0367599477d56b8af5
Jolan <jolan11.dubois@gmail.com>
2025-04-23 11:38:43 +0200

    test: added small test for espn API

---
+++ tests/test_espn_api_client.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import pytest
+import pandas as pd
+
+from src.sources import ESPNApiClient
+
+
+def test__get_matches():
+    """ TODO """
+    api_client = ESPNApiClient()
+    
+    date_strs = ["20250420", "20250524", "20250429"]
+    competition_slugs = ["eng.1", "fra.coupe_de_france", "uefa.champions"]
+    
+    for competition_slug, date_str in zip(competition_slugs, date_strs):
+        matches = api_client._get_matches(competition_slug, date_str)
+
+        assert isinstance(matches, pd.DataFrame), f"Expected DataFrame, got {type(matches)}"
+        assert not matches.empty, f"Expected non-empty DataFrame for {competition_slug} on {date_str}"
