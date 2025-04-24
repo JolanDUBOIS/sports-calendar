515334f32c3ff1b9effcc5eaf5e72d5b8927d865
Jolan <jolan11.dubois@gmail.com>
2025-04-24 15:22:51 +0200

    feat: added gender to competitions and teams & ids instead of name as primary keys

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,7 @@
 import hashlib
 from datetime import datetime, timezone
 
-from sqlalchemy import event, Column, Integer, String, DateTime, ForeignKey, Date, func
+from sqlalchemy import event, Column, Integer, String, DateTime, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,14 +14,17 @@ def generate_hash_id(fields):
 
 class Region(Base):
     __tablename__ = 'regions'
-    name = Column(String, primary_key=True)
+    id = Column(String, primary_key=True)
+    name = Column(String)
 
 
 class Competition(Base):
     __tablename__ = 'competitions'
-    name = Column(String, primary_key=True)
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
     abbreviation = Column(String)
-    region_name = Column(String, ForeignKey('regions.name'))
+    gender = Column(String, nullable=False)  # men/women
+    region_id = Column(String, ForeignKey('regions.id'))
     region = relationship('Region', back_populates="competitions")
     type = Column(String)  # e.g., 'league', 'cup'
 
@@ -31,16 +34,18 @@ Region.competitions = relationship('Competition', back_populates="region")
 
 class Team(Base):
     __tablename__ = 'teams'
-    name = Column(String, primary_key=True)
+    id = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
     abbreviation = Column(String)
+    gender = Column(String, nullable=False)  # men/women
     venue = Column(String)
-    league = Column(String)  # competition with type "league"
+    league_id = Column(String)  # competition with type "league"
 
     def validate_league(self, session):
         """ Method to validate that the league exists in the competitions table """
-        competition = session.query(Competition).filter_by(name=self.league, type='league').first()
+        competition = session.query(Competition).filter_by(id=self.league_id, type='league').first()
         if not competition:
-            raise ValueError(f"League '{self.league}' is not a valid competition.")
+            raise ValueError(f"League '{self.league_id}' is not a valid competition id.")
         return competition
 
     def add_team(self, session):
@@ -58,17 +63,17 @@ class Match(Base):
     date_time = Column(DateTime, nullable=False)
     date = Column(Date, nullable=False)
     time = Column(String, nullable=False)
-    home_team_name = Column(String, ForeignKey('teams.name'), nullable=False)
-    away_team_name = Column(String, ForeignKey('teams.name'), nullable=False)
+    home_team_id = Column(String, ForeignKey('teams.id'), nullable=False)
+    away_team_id = Column(String, ForeignKey('teams.id'), nullable=False)
     venue = Column(String)
-    competition_name = Column(String, ForeignKey('competitions.name'), nullable=False)
+    competition_id = Column(String, ForeignKey('competitions.id'), nullable=False)
     channels = Column(String)
     source = Column(String)
     created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
 
-    home_team = relationship('Team', foreign_keys=[home_team_name])
-    away_team = relationship('Team', foreign_keys=[away_team_name])
-    competition = relationship('Competition')
+    home_team = relationship('Team', foreign_keys=[home_team_id])
+    away_team = relationship('Team', foreign_keys=[away_team_id])
+    competition = relationship('Competition', foreign_keys=[competition_id])
 
     def before_insert(self):
         """ Method to set default values and generate ID before inserting into the database """
@@ -76,9 +81,9 @@ class Match(Base):
             self.date = self.date_time.date()
             self.time = self.date_time.time()
 
-        self.id = generate_hash_id([self.home_team_name, self.away_team_name, self.competition_name, self.date_time, self.created_at])
-        self.match_id = generate_hash_id([self.home_team_name, self.away_team_name, self.competition_name, self.date])
-        self.source_match_id = generate_hash_id([self.home_team_name, self.away_team_name, self.competition_name, self.date, self.source])
+        self.id = generate_hash_id([self.home_team_id, self.away_team_id, self.competition_id, self.date_time, self.created_at])
+        self.match_id = generate_hash_id([self.home_team_id, self.away_team_id, self.competition_id, self.date])
+        self.source_match_id = generate_hash_id([self.home_team_id, self.away_team_id, self.competition_id, self.date, self.source])
 
 
 class Standing(Base):
@@ -86,8 +91,8 @@ class Standing(Base):
     id = Column(String, primary_key=True)
     standing_id = Column(String, nullable=False)
     source_standing_id = Column(String, nullable=False)
-    competition_name = Column(String, ForeignKey('competitions.name'))
-    team_name = Column(String, ForeignKey('teams.name'))
+    competition_id = Column(String, ForeignKey('competitions.id'))
+    team_id = Column(String, ForeignKey('teams.id'))
     position = Column(Integer, nullable=False)
     points = Column(Integer, nullable=False)
     matches_played = Column(Integer)
@@ -100,14 +105,14 @@ class Standing(Base):
     source = Column(String)
     created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
 
-    competition = relationship('Competition')
-    team = relationship('Team')
+    competition = relationship('Competition', foreign_keys=[competition_id])
+    team = relationship('Team', foreign_keys=[team_id])
 
     def before_insert(self):
         """ Method to set default values and generate ID before inserting into the database """
-        self.id = generate_hash_id([self.competition_name, self.team_name, self.created_at])
-        self.standing_id = generate_hash_id([self.competition_name, self.team_name])
-        self.source_standing_id = generate_hash_id([self.competition_name, self.team_name, self.source])
+        self.id = generate_hash_id([self.competition_id, self.team_id, self.created_at])
+        self.standing_id = generate_hash_id([self.competition_id, self.team_id])
+        self.source_standing_id = generate_hash_id([self.competition_id, self.team_id, self.source])
 
 
 @event.listens_for(Match, 'before_insert')
