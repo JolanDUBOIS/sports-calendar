ee6f4bab50c663076cbdcde0c1940826f8b21bfe
Jolan <jolan11.dubois@gmail.com>
2025-05-03 12:14:04 +0200

    feat: added football-anking scraper

---
+++ src/clients/web/football_ranking_scraper.py
@@ -0,0 +1,57 @@
+import re
+
+import pandas as pd
+from bs4 import Tag
+
+from src.clients import logger
+from src.clients.web.base_scraper import BaseScraper
+
+
+class FootballRankingScraper(BaseScraper):
+    """ TODO """
+    
+    base_url = "https://football-ranking.com"
+
+    def __init__(self, **kwargs):
+        """ TODO """
+        super().__init__(**kwargs)
+
+    def scrape_fifa_ranking(self) -> pd.DataFrame:
+        """ TODO """
+        endpoint = "/fifa-rankings"
+        rankings = []
+        for k in range(1,6):
+            url = f"{self.base_url}{endpoint}?page={k}"
+            soup = self.scrape_url(url)
+            if not soup:
+                logger.debug(f"Failed to fetch FIFA ranking page: {url}")
+                continue
+            for row in soup.select("table tbody tr"):
+                parsed_row = self._parse_fifa_ranking(row)
+                if parsed_row:
+                    rankings.append(parsed_row)
+        
+        return pd.DataFrame(rankings)
+    
+    def _parse_fifa_ranking(self, row: Tag) -> dict|None:
+        """ TODO """
+        cols = row.find_all("td")
+        if len(cols) < 5:
+            return None
+
+        rank = cols[0].text.strip()
+        team = cols[1].text.strip()
+        points = cols[2].text.strip()
+
+        rank = re.match(r'\d+', rank)
+        rank = rank.group(0) if rank else ''
+
+        points = points.replace(',', '')
+        points = re.match(r'\d+(\.\d+)?', points)
+        points = points.group(0) if points else ''
+
+        return {
+            "position": rank,
+            "team": team,
+            "points": points
+        }
