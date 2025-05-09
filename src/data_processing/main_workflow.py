bd71ffdeb1a1991631a0cc839fd6ea6b314794a6
Jolan <jolan11.dubois@gmail.com>
2025-05-09 12:57:31 +0200

    feat: added main data processing workflow (run python -m src.data_processing.main_workflow to test the workflow). Work in progress

--- src/data_processing/main_workflow.py
+++ src/data_processing/main_workflow.py
@@ -12,7 +12,7 @@ ORDER_OF_OPERATIONS = {
     "data_extraction": extraction_workflow
 }
 
-def data_processing_workflow(db_repo: str, instructions_path: str|Path, manual: bool = False):
+def data_processing_workflow(db_repo: str, instructions_path: str|Path, workflows: list[str] = None, manual: bool = False):
     """ TODO """
     logger.info("Starting data processing workflow...")
     instructions_path = Path(instructions_path) if isinstance(instructions_path, str) else instructions_path
@@ -27,7 +27,11 @@ def data_processing_workflow(db_repo: str, instructions_path: str|Path, manual: 
         instructions = read_yml_file(path)
         data_processing_instructions[instructions.get('name')] = instructions
 
-    for operation_name, operation_workflow in ORDER_OF_OPERATIONS.items():
+    if workflows:
+        operations = {key: op_workflow for key, op_workflow in ORDER_OF_OPERATIONS.items() if key in workflows}
+    else:
+        operations = ORDER_OF_OPERATIONS
+    for operation_name, operation_workflow in operations.items():
         if operation_name not in data_processing_instructions:
             logger.error(f"Operation {operation_name} is missing in the instructions.")
             raise ValueError(f"Operation {operation_name} is missing in the instructions.")
@@ -41,7 +45,9 @@ def data_processing_workflow(db_repo: str, instructions_path: str|Path, manual: 
 if __name__ == "__main__":
     logger.info("Running data processing workflow...")
     from src.config import get_config
-    db_repo = get_config("db.path")
-    instructions_path = "src/data_processing/instructions"
-    data_processing_workflow(db_repo, instructions_path)
+    db_repo = get_config("pipeline.repository_path")
+    instructions_path = get_config("pipeline.config_files")
+    workflows = get_config("pipeline.workflows")
+    manual = get_config("pipeline.manual")
+    data_processing_workflow(db_repo, instructions_path, workflows=workflows, manual=manual)
     logger.info("Data processing workflow completed.")
--- src/data_processing/utils.py
+++ src/data_processing/utils.py
@@ -1,8 +1,10 @@
 from enum import IntEnum
 from pathlib import Path
+from typing import Any
 from datetime import datetime, timedelta
 
 import yaml
+import pandas as pd
 
 from src.data_processing import logger
 
@@ -25,7 +27,7 @@ class DataStage(IntEnum):
     LANDING = 0
     INTERMEDIATE = 1
     STAGING = 2
-    SERVING = 3
+    PRODUCTION = 3
 
     @classmethod
     def from_str(cls, name: str) -> "DataStage":
@@ -81,6 +83,36 @@ def order_models(models: list[dict], stage: str) -> list[dict]:
         for model in models
         if model.get('name') == ordered_model
     ]
-    logger.debug(f"Final ordered models: {ordered_models}")
     return ordered_models
-                        
+
+
+def inject_static_fields(data: list[dict]|pd.DataFrame, static_fields: dict) -> list[dict]|pd.DataFrame:
+    """ Inject static fields into the data. """
+    if isinstance(data, list):
+        return _inject_static_fields_json(data, static_fields)
+    elif isinstance(data, pd.DataFrame):
+        return _inject_static_fields_df(data, static_fields)
+    else:
+        logger.debug(f"Data type: {type(data)}")
+        logger.debug(f"Static fields: {static_fields}")
+        logger.error("Data should be either a list of dictionaries or a pandas DataFrame.")
+        raise ValueError("Data should be either a list of dictionaries or a pandas DataFrame.")
+
+def _inject_static_fields_json(data: list[dict], static_fields: dict) -> list[dict]:
+    """ Inject static fields into a list of dictionaries. """
+    for item in data:
+        for key, value in static_fields.items():
+            if key not in item:
+                item[key] = value
+            else:
+                logger.warning(f"Static field {key} already exists in the data.")
+    return data
+
+def _inject_static_fields_df(data: pd.DataFrame, static_fields: dict) -> pd.DataFrame:
+    """ Inject static fields into a DataFrame. """
+    for key, value in static_fields.items():
+        if key not in data.columns:
+            data[key] = value
+        else:
+            logger.warning(f"Static field {key} already exists in the data.")
+    return data
