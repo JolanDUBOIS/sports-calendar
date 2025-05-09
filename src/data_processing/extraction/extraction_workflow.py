7f56a909a10e6ef74954e882203334af4cd1609d
Jolan <jolan11.dubois@gmail.com>
2025-05-09 12:56:25 +0200

    refactor: reworked extraction workflow to match new yml structure, use file handlers & manage versioning a bit better

--- src/data_processing/extraction/extraction_workflow.py
+++ src/data_processing/extraction/extraction_workflow.py
@@ -1,84 +1,19 @@
-import json
+from datetime import datetime
 from pathlib import Path
-from abc import ABC, abstractmethod
 
 import pandas as pd
 
 from .extract_json import extract_from_json
 from .extract_tables import extract_parallel_entities
 from src.data_processing import logger
-from src.data_processing.utils import order_models
-
-
-class SourceLoader(ABC):
-    @abstractmethod
-    def load(self, path: str) -> pd.DataFrame:
-        pass
-
-class CSVLoader(SourceLoader):
-    def load(self, path: str) -> pd.DataFrame:
-        return pd.read_csv(path)
-
-class JSONLoader(SourceLoader):
-    def load(self, path: str) -> dict:
-        with open(path, "r") as f:
-            data = json.load(f)
-        return data
-
-LOADER_REGISTRY = {
-    "csv": CSVLoader(),
-    "json": JSONLoader(),
-}
+from src.data_processing.utils import order_models, inject_static_fields
+from src.data_processing.file_io import FileHandlerFactory
 
 
 EXTRACTION_FUNCTION_REGISTRY = {
     "extract_from_json": extract_from_json,
     "extract_parallel_entities": extract_parallel_entities
 }
-
-def load_sources(db_repo: Path, sources: list[dict]) -> dict[str, pd.DataFrame|dict]:
-    """ TODO """
-    loaded_sources = {}
-    for source in sources:
-        source_name = source.get("name")
-        source_type = source.get("type")
-        source_path = source.get("path")
-        
-        loader = LOADER_REGISTRY.get(source_type)
-        if not loader:
-            logger.error(f"Unsupported source type: {source_type}.")
-            raise ValueError(f"Unsupported source type: {source_type}.")
-        
-        loaded_sources[source_name] = loader.load(db_repo / source_path)
-
-    return loaded_sources
-
-def validate_table(data: pd.DataFrame, columns: list[dict]) -> pd.DataFrame:
-    """ TODO """
-    if columns is None:
-        logger.error(f"Columns mapping is missing in instruction file.")
-        raise ValueError(f"Columns mapping is missing in instruction file.")
-
-    col_names = []
-    for column in columns:
-        if not isinstance(column, dict):
-            logger.error(f"Column mapping should be a dictionary.")
-            raise ValueError(f"Column mapping should be a dictionary.")
-        col_name = column.get("name")
-        if col_name not in data.columns:
-            logger.warning(f"Column {col_name} is missing, creating empty column.")
-            data[col_name] = None
-        static_value = column.get("static")
-        if static_value:
-            data[col_name] = static_value
-        col_names.append(col_name)
-
-    if set(col_names) != set(data.columns):
-        # TODO - Find better error msg
-        logger.error(f"Output columns do not match the expected columns.")
-        raise ValueError(f"Output columns do not match the expected columns.")
-
-    return data
 
 def save_file(file_path: Path, data: pd.DataFrame):
     """ TODO """
@@ -102,6 +37,7 @@ def extraction_workflow(db_repo: str, instructions: dict, manual: bool = False):
     models = instructions.get("models", [])
     models = order_models(models, "intermediate")
     for model in models:
+        # Trigger
         model_trigger = model.get("trigger", "automatic")
         if not manual and model_trigger == "manual":
             continue
@@ -111,10 +47,37 @@ def extraction_workflow(db_repo: str, instructions: dict, manual: bool = False):
 
         model_name = model.get("name")
         logger.info(f"Extraction for model: {model_name}")
+        
+        # Output handler
+        output = model.get("output", {})
+        output_handler = FileHandlerFactory.create_file_handler(db_repo / output.get("path"))
+        output_handler.add_schema(output.get("columns", []))
+        output_last_update = output_handler.last_update()
 
-        loaded_sources = load_sources(db_repo, model.get("sources", []))
+        # Source handlers
+        sources = model.get("sources", [])
+        loaded_sources = {}
+        for source in sources:
+            source_name = source.get("name")
+            source_path = source.get("path")
+            source_versioning = source.get("versioning", {})
+            version_mode = source_versioning.get("mode", "all")
+            version_on = source_versioning.get("on", "created_at")
+            version_type = source_versioning.get("version_type", "datetime")
 
+            file_handler = FileHandlerFactory.create_file_handler(db_repo / source_path)
+            loaded_sources[source_name] = file_handler.read(
+                mode=version_mode,
+                on=version_on,
+                version_threshold=output_last_update,
+                version_type=version_type
+            )
+        logger.debug(f"Loaded sources: {loaded_sources.keys()}")
+        logger.debug(f"Loaded sources data: {loaded_sources}")
+
+        # Extraction
         extraction_func = EXTRACTION_FUNCTION_REGISTRY.get(model.get("function"))
+        logger.debug(f"Extraction function: {extraction_func}")
         if not extraction_func:
             logger.error(f"Unsupported extraction function: {model.get('function')}.")
             raise ValueError(f"Unsupported extraction function: {model.get('function')}.")
@@ -122,16 +85,16 @@ def extraction_workflow(db_repo: str, instructions: dict, manual: bool = False):
 
         output = pd.DataFrame()
         for params in list_params:
+            logger.debug(f"Extraction params: {params}")
             new_output = extraction_func(sources = loaded_sources, **params)
+            logger.debug(f"New output: {new_output}")
             if not isinstance(output, pd.DataFrame):
                 logger.error(f"Output from extraction function is not a DataFrame.")
                 raise ValueError(f"Output from extraction function is not a DataFrame.")
-
-            columns = model.get("columns")
-            new_output = validate_table(new_output, columns)
-
             output = pd.concat([output, new_output], ignore_index=True)
 
-        output_file = db_repo / model.get("output")
-        save_file(output_file, output)
+        static_fields = model.get("static_fields", [])
+        output = inject_static_fields(output, static_fields)        
+        output_handler.write(data=output, overwrite=output.get("overwrite", False))
+
     logger.info("Extraction workflow completed.")
