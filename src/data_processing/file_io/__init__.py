2e8a565f6b63ad0a42fdf0b568304ca6b4c56593
Jolan <jolan11.dubois@gmail.com>
2025-05-14 22:41:56 +0200

    chore: added import

--- src/data_processing/file_io/__init__.py
+++ src/data_processing/file_io/__init__.py
@@ -4,3 +4,5 @@ logger = logging.getLogger(__name__)
 
 from .file_handler import FileHandler
 from .file_handler_factory import FileHandlerFactory
+from .csv_handler import CSVHandler
+from .json_handler import JSONHandler
