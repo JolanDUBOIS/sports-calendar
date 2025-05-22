758b872e0da2c70b0ab544e437c6b6ae5943a4f9
Jolan <jolan11.dubois@gmail.com>
2025-05-22 18:08:38 +0200

    chore: readapted main modules to adapt to updated workflow

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,7 @@
 from dotenv import load_dotenv
 
-from src.main import run_selection, test
+from src.main import test
 from src.config import parse_arguments
-# from src.sources import update_database
 
 
 load_dotenv()
@@ -11,14 +10,13 @@ if __name__ == '__main__':
     args = parse_arguments()
 
     if args.update_database:
-        # update_database()
-        pass
-    
+        raise NotImplementedError("Database update is not implemented yet.")
+
     elif args.full_update:
-        pass
+        raise NotImplementedError("Full update is not implemented yet.")
 
     elif args.run_selection:
-        run_selection(save_ics=True)
+        raise NotImplementedError("Run selection is not implemented yet.")
 
     elif args.test:
         test(args.test)
--- src/main.py
+++ src/main.py
@@ -1,63 +1,41 @@
 from pathlib import Path
-# from datetime import datetime
 
 from src import logger
-# from src.config import get_config
-# from src.selection import Selection
-# from src.sources import DatabaseManager, FootballRankingScraper, update_database
-# from src.calendar import FootballCalendar, GoogleCalendarManager
 from src.data_pipeline.utils import read_yml_file
-from src.data_pipeline.data_processing.ingestion_landing import ingestion_landing
-from src.data_pipeline.data_processing.build_intermediate import build_intermediate
+from src.data_pipeline.data_processing import build_layer
 
-
-def run_selection(save_ics: bool=False):
-    """ TODO """
-    logger.info("Running selection...")
-
-#     db = DatabaseManager()
-#     calendar = FootballCalendar()
-#     google_calendar_manager = GoogleCalendarManager()
-
-#     selection_file_path = Path(get_config("selection.file_path"))
-#     selection = Selection.from_json(selection_file_path, db)
-#     matches = selection.get_matches()
-#     calendar.add_matches(matches)
-#     if save_ics:
-#         now = datetime.now().strftime("%Y-%m-%d")
-#         calendar.save_to_ics(Path("data") / "ics_calendars" / f"selection_calendar_{now}.ics") # TODO - Add to config
-#     google_calendar_manager.add_calendar(calendar)
-#     logger.info("Selection ran successfully")
-
-# TODO - update_database should be in this script !!
 
 def test(no: int):
     """ TODO """
     logger.info("Running test...")
     if no == 1:
-        db_repo = Path(__file__).parent.parent / "data/repository"
-        instructions_path = Path(__file__).parent.parent / "config/pipeline_config/daily/workflows/ingestion_landing.yml"
-        instructions = read_yml_file(instructions_path)
-        ingestion_landing(db_repo, instructions, manual=True)
-        
+        logger.info("Running build_landing test...")
+        db_repo = Path("data/repository/test")
+        workflow_instructions = read_yml_file(Path("config/pipeline_config/test/workflows/build_landing.yml"))
+        build_layer(db_repo, workflow_instructions)
+
     elif no == 2:
-        db_repo = Path(__file__).parent.parent / "data/repository"
-        instructions_path = Path(__file__).parent.parent / "config/pipeline_config/daily/workflows/build_intermediate.yml"
-        instructions = read_yml_file(instructions_path)
-        build_intermediate(db_repo, instructions, manual=True)
+        logger.info("Running build_intermediate test...")
+        db_repo = Path("data/repository/test")
+        intermediate_dir = db_repo / "intermediate"
+        for file in intermediate_dir.glob("*"):
+            if file.is_file():
+                file.unlink()
+        workflow_instructions = read_yml_file(Path("config/pipeline_config/test/workflows/build_intermediate.yml"))
+        build_layer(db_repo, workflow_instructions)
 
-    # elif no == 3:
-    #     selection_file_path = Path("data/selections/new_selection.json")
-    #     db = DatabaseManager()
-    #     selection = Selection.from_json(selection_file_path, db)
-    #     matches = selection.get_matches()
-    #     logger.debug(matches)
-
-    # elif no == 4:
-    #     scraper = FootballRankingScraper()
-    #     rankings = scraper.get_fifa_rankings()
-    #     logger.debug(rankings)
-
-    # elif no == 5:
-    #     competitions = ["Ligue 1"]
-    #     update_database(competitions)
+    elif no == 3:
+        logger.info("Running the true build_landing...")
+        db_repo = Path("data/repository")
+        workflow_instructions = read_yml_file(Path("config/pipeline_config/daily/workflows/build_landing.yml"))
+        build_layer(db_repo, workflow_instructions)
+    
+        # Temporary
+        landing_dir = db_repo / "landing"
+        test_landing_dir = Path("data/repository/test/landing")
+        for file in test_landing_dir.glob("*"):
+            if file.is_file():
+                file.unlink()
+        for file in landing_dir.glob("*"):
+            if file.is_file():
+                file.copy(test_landing_dir / file.name)
