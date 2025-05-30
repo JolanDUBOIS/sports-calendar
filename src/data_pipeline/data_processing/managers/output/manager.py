9f276959402b11f1c8d69ba09b01f9c0ea3ba515
Jolan <jolan11.dubois@gmail.com>
2025-05-30 12:42:38 +0200

    fix: fixed output manager writing so that non-nullable and unique are applied to the whole output table

--- src/data_pipeline/data_processing/managers/output/manager.py
+++ src/data_pipeline/data_processing/managers/output/manager.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from . import logger
 from .specs import OutputSpec
 from .enforcers import UniqueEnforcer, NonNullableEnforcer
 from ...versioning import read_versions
+from ....utils import concat_io_content
 from ....file_io import FileHandlerFactory
 
 if TYPE_CHECKING:
@@ -17,6 +19,7 @@ class OutputManager:
 
     def __init__(self, output_spec: OutputSpec):
         """ TODO """
+        logger.debug(f"Initializing OutputManager with spec: {output_spec}")
         self.output_spec = output_spec
         self.handler = FileHandlerFactory.create_file_handler(self.output_spec.path, tracked=True)
         self.enforcers = self._init_enforcers()
@@ -31,14 +34,22 @@ class OutputManager:
         return enforcers
 
     def write(self, data: IOContent, source_versions: SourceVersions):
-        """ Write data to the output file. """
-        for enforcer in self.enforcers:
-            data = enforcer.apply(data)
+        """ TODO """
+        try:
+            existing_data = self.handler.read()
+            full_data = concat_io_content(existing_data, data)
+        except FileNotFoundError:
+            logger.debug("No existing output file found. Proceeding with new data only.")
+            full_data = data
 
-        self.handler.write(data, source_versions=source_versions.to_dict())
+        for enforcer in self.enforcers:
+            logger.debug(f"Applying enforcer: {enforcer.__class__.__name__}")
+            full_data = enforcer.apply(full_data)
+
+        self.handler.write(full_data, source_versions=source_versions.to_dict(), overwrite=True)
         self.handler.save()
 
     def read_source_versions(self) -> SourceVersions:
         """ Read source versions from the output metadata. """
-        metadata_entries = self.handler.meta_manager.read()
-        return read_versions(metadata_entries)
+        metadata_entry = self.handler.meta_manager.read_last_write()
+        return read_versions(metadata_entry)
