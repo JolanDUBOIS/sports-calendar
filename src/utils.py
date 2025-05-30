8b80d61117c98f386532632dd9475452d0ac592a
Jolan <jolan11.dubois@gmail.com>
2025-05-30 15:15:19 +0200

    docs: added docstring to concatenate_unique_rows

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ def concatenate_unique_rows(
     events2: pd.DataFrame,
     columns_to_compare: list[str] = None
 ) -> pd.DataFrame:
-    """ TODO """
+    """ Concatenate two DataFrames and remove duplicates based on specified columns. """
     concatenated_df = pd.concat([events1, events2], ignore_index=True)
     concatenated_df.drop_duplicates(inplace=True, subset=columns_to_compare)
     return concatenated_df
