b8148e930fdda44025fd4618cdde29483d29156a
Jolan <jolan11.dubois@gmail.com>
2025-06-11 11:03:36 +0200

    feat: made processor factory more extendable

--- src/data_pipeline/data_processing/processors/processor_factory.py
+++ src/data_pipeline/data_processing/processors/processor_factory.py
@@ -1,20 +1,31 @@
+from . import logger
 from .processor_base_class import Processor
-from .landing.client import ClientProcessor
-from .intermediate import DerivationProcessor, ExtractionProcessor, ParsingProcessor, SimilarityProcessor
+from .landing import *
+from .intermediate import *
 
 
 class ProcessorFactory:
     """ Factory class to create processor instances by name. """
 
-    processors = {
-        "DerivationProcessor": DerivationProcessor,
-        "ExtractionProcessor": ExtractionProcessor,
-        "ClientProcessor": ClientProcessor,
-        "ParsingProcessor": ParsingProcessor,
-        "SimilarityProcessor": SimilarityProcessor,
-    }
+    @classmethod
+    def _all_processors(cls) -> dict[str, type[Processor]]:
+        """Discover all subclasses of Processor and return them as a name-to-class map."""
+        def all_subclasses(cls_):
+            return set(cls_.__subclasses__()).union(
+                s for c in cls_.__subclasses__() for s in all_subclasses(c)
+            )
+
+        return {
+            subclass.__name__: subclass
+            for subclass in all_subclasses(Processor)
+        }
 
     @classmethod
     def get_processor(cls, name: str) -> Processor:
-        """ Return a processor instance based on the given name. """
-        return cls.processors.get(name)()
+        """Return a processor instance by class name."""
+        processors = cls._all_processors()
+        logger.debug(f"Available processors: {list(processors.keys())}")
+        if name not in processors:
+            logger.error(f"Processor '{name}' not found in factory.")
+            raise ValueError(f"Processor '{name}' not found in factory.")
+        return processors[name]()
