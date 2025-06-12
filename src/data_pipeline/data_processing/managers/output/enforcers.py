9003e7021ed1fd370e03d32aa057f41394bc90e7
Jolan <jolan11.dubois@gmail.com>
2025-06-12 20:06:17 +0200

    feat: small refactor of enforcers

--- src/data_pipeline/data_processing/managers/output/enforcers.py
+++ src/data_pipeline/data_processing/managers/output/enforcers.py
@@ -10,13 +10,23 @@ from .specs import UniqueSpec, NonNullableSpec
 class OutputEnforcer(ABC):
     """ Abstract base class for enforcing constraints on output DataFrames. """
 
-    @abstractmethod
+    def __init__(self, spec: UniqueSpec | NonNullableSpec):
+        """ Initialize the enforcer with the specification. """
+        self.spec = spec
+
     def apply(self, df: pd.DataFrame) -> pd.DataFrame:
         """ Apply the constraint enforcement to the given DataFrame. """
+        self._check_df(df)
+        return self._apply(df)
+
+    @abstractmethod
+    def _apply(self, df: pd.DataFrame) -> pd.DataFrame:
+        """ Apply the specific enforcement logic to the DataFrame. """
+        raise NotImplementedError("Subclasses must implement this method.")
 
     @staticmethod
     def _check_df(df: pd.DataFrame) -> None:
-        """ Check if df is a valid DataFrame, raise error if not. """
+        """ Check if df is valid, raise error if not. """
         if not isinstance(df, pd.DataFrame):
             logger.error("Input is not a valid DataFrame.")
             raise ValueError("Input is not a valid DataFrame.")
@@ -24,13 +34,8 @@ class OutputEnforcer(ABC):
 
 class UniqueEnforcer(OutputEnforcer):
     """ Enforces uniqueness in the DataFrame based on specified fields. """
-
-    def __init__(self, spec: UniqueSpec):
-        """ Initialize the enforcer with the specification. """
-        self.spec = spec
-
-    def apply(self, df: pd.DataFrame) -> pd.DataFrame:
-        """ Apply uniqueness enforcement to the DataFrame. """
+    def _apply(self, df: pd.DataFrame) -> pd.DataFrame:
+        """ Apply the specific enforcement logic to the DataFrame. """
         for field_set in self.spec.field_sets:
             logger.debug(f"Enforcing uniqueness for field set: {field_set}")
             df = df.sort_values(by=self.spec.version_col, ascending=True).drop_duplicates(subset=field_set, keep=self.spec.keep)
@@ -40,12 +45,8 @@ class UniqueEnforcer(OutputEnforcer):
 class NonNullableEnforcer(OutputEnforcer):
     """ Enforces non-nullability in the DataFrame based on specified fields. """
 
-    def __init__(self, spec: NonNullableSpec):
-        """ Initialize the enforcer with the specification. """
-        self.spec = spec
-
-    def apply(self, df: pd.DataFrame) -> pd.DataFrame:
-        """ Apply non-nullability enforcement to the DataFrame. """
+    def _apply(self, df: pd.DataFrame) -> pd.DataFrame:
+        """ Apply the specific enforcement logic to the DataFrame. """
         pd.set_option('future.no_silent_downcasting', True)
         df = df.replace({"nan": np.nan, "None": np.nan, "": np.nan})
         return df.dropna(subset=self.spec.fields).reset_index(drop=True)
