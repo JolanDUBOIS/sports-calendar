89a873a9ba30fb2e1b1ecc9e50ae646802ef2ef7
Jolan <jolan11.dubois@gmail.com>
2025-06-17 23:45:09 +0200

    fix: added yaml_loader to root init

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,15 @@
 from src.config import setup_logging
 
+# 1. Setup logging
 setup_logging('config/logging_config.yml')
 
-import logging
+# 2. Register custom YAML constructors
+from .config import yaml_loader
 
+# 3. Setup the default logger
+import logging
 logger = logging.getLogger()
 
+# 4. Define root path
 from pathlib import Path
-
 ROOT_PATH = Path(__file__).parent.parent
