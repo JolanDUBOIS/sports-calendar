aee112072405cff3acd8ce804ae2d6c5f60d8601
Jolan <jolan11.dubois@gmail.com>
2025-06-17 23:47:59 +0200

    chore: import resolution

--- src/data_pipeline/__init__.py
+++ src/data_pipeline/__init__.py
@@ -3,6 +3,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 from .pipeline_stages import DataStage
-from .pipeline_orchestration import PipelineConfig
+from .orchestration import PipelineConfig
 from .data_processing import run_pipeline
 from .data_validation import run_validation, SchemaValidationResult
