5004c3a3fc3a5ba5b29efd427cecbdb7c66ae917
Jolan <jolan11.dubois@gmail.com>
2025-06-19 11:32:11 +0200

    feat: rework of the orchestration functions following the new config tools (much better now, less coupling, clearer code)

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from . import logger
 from .data_access import get_clean_matches
 from .calendar import (
@@ -5,27 +8,18 @@ from .calendar import (
     MatchesDetails,
     GoogleCalendarManager
 )
-from .orchestration import AppConfig
+from .selection import SelectionManager
+from src.config.manager import secrets
+if TYPE_CHECKING:
+    from src.config.base_config import Repository
 
 
-def run_selection(repo: str = "prod", name: str = "dev", dry_run: bool = False, preset: str = "default", **kwargs):
+def run_selection(key: str = "dev", dry_run: bool = False, **kwargs):
     """ TODO """
-    logger.info(f"Running selection for selection {name}.")
+    logger.info(f"Running selection for selection {key}.")
 
-    app_config = AppConfig(repo=repo)
-    selection = app_config.get_selection(name)
-    selection_params = app_config.get_selection_params(key=preset)
-    gcal_id = app_config.get_gcal_id(key=name)
-
-    if selection is None:
-        logger.error(f"Selection {name} not found in the configuration.")
-        raise ValueError(f"Selection {name} not found in the configuration.")
-    if selection_params is None:
-        logger.error(f"Selection parameters for {preset} not found in the configuration.")
-        raise ValueError(f"Selection parameters for {preset} not found in the configuration.")
-    if gcal_id is None:
-        logger.error(f"Google Calendar ID for {selection.username} not found in the secrets.")
-        raise ValueError(f"Google Calendar ID for {selection.username} not found in the secrets.")
+    selection = SelectionManager().get_selection(key)
+    gcal_id = secrets.get_gcal_id(key)
 
     matches = selection.get_matches()
     matches = get_clean_matches(matches)
@@ -39,16 +33,17 @@ def run_selection(repo: str = "prod", name: str = "dev", dry_run: bool = False, 
         return
 
     google_cal_manager = GoogleCalendarManager.from_defaults(gcal_id)
+    google_cal_manager.clear_calendar(scope='future', verbose=True)
     google_cal_manager.add_calendar(football_calendar.calendar, verbose=True)
 
-    logger.info(f"Selection {name} has been successfully processed and added to the google calendar.")
+    logger.info(f"Selection {key} has been successfully processed and added to the google calendar.")
 
-def clear_calendar(scope: str = 'future', key: str = "dev", **kwargs):
+def clear_calendar(key: str = "dev", scope: str | None = None, date_from: str | None = None, date_to: str | None = None, **kwargs):
     """ Clear events from the Google Calendar based on the specified scope. """
     logger.info(f"Clearing calendar events with scope: {scope}")
 
-    gcal_id = AppConfig().get_gcal_id(key=key)
+    gcal_id = secrets.get_gcal_id(key)
     google_cal_manager = GoogleCalendarManager.from_defaults(gcal_id)
-    google_cal_manager.clear_calendar(scope=scope, verbose=True)
+    google_cal_manager.clear_calendar(scope=scope, date_from=date_from, date_to=date_to, verbose=True)
 
     logger.info(f"Calendar events cleared successfully.")
