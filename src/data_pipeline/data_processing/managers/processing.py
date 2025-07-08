a41ea799ccd3a9528948b5dc7a72b308d3afb92e
Jolan <jolan11.dubois@gmail.com>
2025-07-08 22:10:27 +0200

    feat: adapted the code to the new specs (it's now clearer and better separated)

--- src/data_pipeline/data_processing/managers/processing.py
+++ src/data_pipeline/data_processing/managers/processing.py
@@ -1,40 +1,25 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any
-from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 from . import logger
 from ..processors import ProcessorFactory
+from src.types import IOContent
 
 if TYPE_CHECKING:
-    from ....types import IOContent
+    from src.specs import ProcessingStepSpec
 
-
-@dataclass
-class ProcessingSpec:
-    """ Describes how to process the data (e.g., transformations, filters). """
-    processor_name: str
-    parameters: dict[str, Any]
-
-    @classmethod
-    def from_dict(cls, d: dict) -> ProcessingSpec:
-        """ Create a ProcessingSpec from a dictionary. """
-        return cls(
-            processor_name=d["processor"],
-            parameters={k: v for k, v in d.items() if k != "processor"}
-        )
 
 class ProcessingManager:
-    """ Manage data processing using a specified processing specification. """
+    """ Manage data processing using a processing specification. """
 
-    def __init__(self, processing_spec: ProcessingSpec):
+    def __init__(self, processing_spec: list[ProcessingStepSpec]):
         """ Initialize with a processing specification. """
         self.processing_spec = processing_spec
 
-    def process(self, sources: dict[str, IOContent]) -> IOContent:
+    def process(self, sources: dict[str, IOContent]) -> dict[str, IOContent]:
         """ Process the data according to the processing specification. """
-        processor = ProcessorFactory.get_processor(self.processing_spec.processor_name)
-        logger.debug(f"Using processor: {processor.__class__.__name__} with parameters: {self.processing_spec.parameters}")
-        return processor.run(
-            sources=sources,
-            **self.processing_spec.parameters
-        )
+        data = sources.copy()
+        for step in self.processing_spec:
+            processor = ProcessorFactory.create_processor(step.processor)
+            data = processor.run(data=data, processing_step_spec=step)
+        return data
