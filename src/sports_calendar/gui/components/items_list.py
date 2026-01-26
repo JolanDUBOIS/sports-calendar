ed58d54893f6e0a04f2f8433017b98f4d53ab317
JolanDUBOIS <jolan11.dubois@gmail.com>
2026-01-26 23:01:27 +0100

    feat: expansion of items is saved between reloads to improve greatly UX

--- src/sports_calendar/gui/components/items_list.py
+++ src/sports_calendar/gui/components/items_list.py
@@ -1,10 +1,12 @@
+from cmath import exp
 from nicegui import ui
 
 from . import logger
 from .modals import Modal
 from .filters import filters_list
+from .filters.fields import TextField
 from .filters.filter_modal import open_filter_modal
-from sports_calendar.core.selection import SelectionService, Selection, SelectionItem, SelectionFilter
+from sports_calendar.core.selection import SelectionService, Selection, SelectionItem
 
 
 def items_list(selection: Selection):
@@ -18,14 +20,13 @@ def items_list(selection: Selection):
         for item in items:
             item_card(selection.name, item)
 
+
+expansion_state = {} # Keeps track of expansion states of items
+
 def item_card(selection_name: str, item: SelectionItem):
     n_filters = len(item.filters)
-
-    def on_delete_click():
-        logger.debug(f"Delete clicked for item {item.uid} in selection {selection_name}")
-        Modal(f'Are you sure you want to delete this item?', confirm_color='red', reload_on_confirm=True).open(
-            lambda: SelectionService.remove_item(selection_name, item.uid)
-        )
+    
+    is_open = expansion_state.get(item.uid, False)
 
     def on_add_filter_click():
         logger.debug(f"Add Filter clicked for item {item.uid} in selection {selection_name}")
@@ -46,20 +47,43 @@ def item_card(selection_name: str, item: SelectionItem):
             on_confirm_callback=on_confirm
         )
 
-    with ui.expansion().classes('w-full bg-white rounded shadow') as exp:
+    def on_edit_click():
+        logger.debug(f"Edit clicked for item {item.uid} in selection {selection_name}")
+        Modal(
+            "Edit Item's name:",
+            min_width='500px',
+            max_width='800px',
+            reload_on_confirm=True
+        ).open(
+            lambda new_name: SelectionService.rename_item(
+                selection_name=selection_name,
+                item_uid=item.uid,
+                new_name=new_name
+            ),
+            fields=[TextField('new_name', 'New Name', default=item.name)]
+        )
+
+    def on_delete_click():
+        logger.debug(f"Delete clicked for item {item.uid} in selection {selection_name}")
+        Modal(f'Are you sure you want to delete this item?', confirm_color='red', reload_on_confirm=True).open(
+            lambda: SelectionService.remove_item(selection_name, item.uid)
+        )
+
+    with ui.expansion(value=is_open).classes('w-full bg-white rounded shadow') as exp:
 
         # HEADER
         with exp.add_slot('header'):
             with ui.row().classes('w-full items-center justify-between'):
-                ui.label(
-                    f'{item.sport.capitalize()} ({n_filters} filters)'
-                ).classes('font-bold')
-
+                ui.label(f'{item.name} - {item.sport.capitalize()} ({n_filters} filters)').classes('font-bold')
                 ui.label(item.uid).classes('text-sm italic text-gray-500')
 
                 with ui.row().classes('gap-2 ml-auto'):
                     ui.button('+ Filter').props('flat dense small').on('click.stop', on_add_filter_click)
+                    ui.button('Edit').props('flat dense small').on('click.stop', on_edit_click)
                     ui.button('Delete').props('color=red flat dense small').on('click.stop', on_delete_click)
 
         # BODY
         filters_list(selection_name, item)
+
+        # Update expansion state on toggle
+        exp.on('update:model-value', lambda _: expansion_state.update({item.uid: exp.value}))
