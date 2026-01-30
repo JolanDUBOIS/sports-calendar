cf21346fb5f16258798c6cf91a87a141d0362c6f
JolanDUBOIS <jolan11.dubois@gmail.com>
2026-01-30 18:25:24 +0100

    chore: removed all core.db

--- src/sports_calendar/core/db/football/__init__.py
+++
@@ -1,5 +0,0 @@
-from .. import logger
-from .competitions import FootballCompetitionsTable
-from .matches import FootballMatchesTable
-from .standings import FootballStandingsTable
-from .teams import FootballTeamsTable
