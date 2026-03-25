bce0e85bf5f77bbb98205719e3de2877f86fcc20
JolanDUBOIS <jolan11.dubois@gmail.com>
2026-03-25 19:36:59 +0100

    chore: imports

--- src/sports_calendar/core/selection/__init__.py
+++ src/sports_calendar/core/selection/__init__.py
@@ -3,5 +3,9 @@ logger = logging.getLogger(__name__)
 
 from .models import Selection, SelectionItem
 from .service import SelectionService
-from .engine import SelectionApplier
-from .filters import SelectionFilter, FilterType
+from .filters import (
+    SelectionFilter, FilterType, Rule, EntitySelectionRule,
+    FilterFields, EmptyFilterFields, MinRankingFilterFields,
+    CompetitionsFilterFields, TeamsFilterFields, SessionsFilterFields,
+    FilterTarget, FILTER_DEFINITIONS
+)
